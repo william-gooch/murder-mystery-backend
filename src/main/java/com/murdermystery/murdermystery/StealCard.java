f2f124d960664fd4b93886e2605b9921db36b2ab
William Gooch <william.gooch.wg@gmail.com>
2022-03-05 23:09:05 +0000

    Add package to StealCard

--- src/main/java/com/murdermystery/murdermystery/StealCard.java
+++ src/main/java/com/murdermystery/murdermystery/StealCard.java
@@ -1,19 +1,21 @@
-public class StealCard extends Card{
+package com.murdermystery.murdermystery;
+
+public class StealCard extends Card {
 
     public StealCard(int id) {
         this.id = id;
         name = "Steal Card";
         desc = "Allows the player to steal a card from another player";
     }
+
     public Boolean steal(Boolean isDay, Player stealer, Player victim, int toSteal) {
         if (isDay == true) {
             return false;
-        }
-        else if (victim.getDeck().size() == 0) {
+        } else if (victim.getDeck().size() == 0) {
             return false;
         }
         Card stolen = victim.getDeck().get(toSteal);
-        victim.removeCard(stolen);
+        victim.getDeck().remove(stolen);
         stealer.addCard(stolen);
         return true;
     }
