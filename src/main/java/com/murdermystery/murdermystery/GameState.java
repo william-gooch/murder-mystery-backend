194ec179b2a8aef75465539e9a6b6175ca4b21e8
ws58 <ws58@st-andrews.ac.uk>
2022-03-05 17:19:28 +0000

    something

--- src/main/java/com/murdermystery/murdermystery/GameState.java
+++ src/main/java/com/murdermystery/murdermystery/GameState.java
@@ -1,6 +1,8 @@
 package com.murdermystery.murdermystery;
 import java.util.concurrent.ThreadLocalRandom;
 
+import com.murdermystery.murdermystery.Player;
+
 public class GameState {
 
     // private static final int TEST_DECK = 50; //allows the deck to be created for now
@@ -11,34 +13,29 @@ public class GameState {
     private int deckPointer = TEST_DECK - 1;
     private int initHand = 3;
 
-<<<<<<< HEAD
     public void GameState() { //will be received from the front end
     }
     public void setUpPlayers(int noPlayers) {
         //create list of players
-=======
-
-
-    public GameState(int noPlayers) { //will be received from the front end
->>>>>>> f4b859d9d1f0b31d8e4b9c0176cd290265de1b53
         players = new Player[noPlayers];
         // use below to find murderer
         int randomNum = ThreadLocalRandom.current().nextInt(0, noPlayers);  //https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
         //give the players their name
-        for(int i = 0; i < noPlayers; i++) {
-            players[i] = new Player(5000+i,TEST_NAME); //will be received from the front end
+        for (Player player : players) {
+            for(int i = 0; i < noPlayers; i++) {
+                players[i] = new Player(5000+i,TEST_NAME); //will be received from the front end
             if (i == randomNum) {
-                player.isMurderer = true;
+                player.setIsMurderer(true);
             }
             else {
-                player.isMurderer = false;
+                player.setIsMurderer(false);
             }
             for (int j = 0; j < initHand; j++) {
-                
+                // players.
+            }
             }
         }
 
-<<<<<<< HEAD
         //deal cards to players
     }
     // //https://stackoverflow.com/questions/1519736/random-shuffling-of-an-array
@@ -51,17 +48,5 @@ public class GameState {
             fullDeck[index] = fullDeck[i];
             fullDeck[i] = a;
         }
-=======
-    public GameState() {
-        
-    }    
-    
-    public Player[] getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(Player[] players) {
-        this.players = players;
->>>>>>> f4b859d9d1f0b31d8e4b9c0176cd290265de1b53
     }
 }   
