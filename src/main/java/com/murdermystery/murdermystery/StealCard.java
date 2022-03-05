5a5721fa41cadd38243454a204a623f499f19237
ws58 <ws58@st-andrews.ac.uk>
2022-03-05 23:00:12 +0000

    Steal!

---
+++ src/main/java/com/murdermystery/murdermystery/StealCard.java
@@ -0,0 +1,20 @@
+public class StealCard extends Card{
+
+    public StealCard(int id) {
+        this.id = id;
+        name = "Steal Card";
+        desc = "Allows the player to steal a card from another player";
+    }
+    public Boolean steal(Boolean isDay, Player stealer, Player victim, int toSteal) {
+        if (isDay == true) {
+            return false;
+        }
+        else if (victim.getDeck().size() == 0) {
+            return false;
+        }
+        Card stolen = victim.getDeck().get(toSteal);
+        victim.removeCard(stolen);
+        stealer.addCard(stolen);
+        return true;
+    }
+}
