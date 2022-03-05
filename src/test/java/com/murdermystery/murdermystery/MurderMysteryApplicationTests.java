68a3f2b755dc9827ecaf34099e06ef67b5a02fdc
William Gooch <william.gooch.wg@gmail.com>
2022-03-05 13:04:10 +0000

    Spring boot initialization.

---
+++ src/test/java/com/murdermystery/murdermystery/MurderMysteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murdermystery.murdermystery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurderMysteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
