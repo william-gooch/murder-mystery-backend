9e10035f07bbd1b11e0e5fb9a7d6839d649728be
William Gooch <william.gooch.wg@gmail.com>
2022-03-05 15:50:02 +0000

    Websocket Spring stuff

---
+++ src/main/java/com/murdermystery/murdermystery/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.murdermystery.murdermystery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler(), "/user");
+    }
+
+    @Bean
+    public WebSocketHandler handler() {
+        return new GameSocketHandler();
+    }
+}
